af7e5fd477e2a2c12569b6fd2504e5453c0391b9
Pedro Oliveira <pedro_oliva@outlook.com.br>
2025-10-07 14:52:14 -0300

    tests
    tests

---
+++ rental-company/src/test/java/io/github/spring/osrb2/rental_company/RentalCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.spring.osrb2.rental_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
